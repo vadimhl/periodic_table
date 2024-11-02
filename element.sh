38bd17a611cfd650070ad3794d8272b372a0f363
vadimhl <vadimhl@gmail.com>
2024-11-02 14:19:38 +0200

    feat: print info by name or symbol

--- element.sh
+++ element.sh
@@ -5,13 +5,13 @@ PSQL="psql -X -A --dbname=periodic_table -t --no-align -c"
 
 ELEMENT_INFO(){
    
-  DATA=$($PSQL  "select 'The element with atomic number ' || atomic_number || ' is ' || name || ' (' || symbol || '). It''s a ' || type 
-   || ', with a mass of ' || atomic_mass || ' amu. ' || name 
-   || ' has a melting point of ' || melting_point_celsius || ' celsius and a boiling point of ' 
-   || boiling_point_celsius || ' celsius.'
-                     from elements left join properties using(atomic_number) 
-                                 left join types using(type_id) 
-                   where atomic_number =  $1")
+  DATA=$($PSQL "select 'The element with atomic number ' || atomic_number || ' is ' || name || ' (' || symbol || '). It''s a ' || type 
+                        || ', with a mass of ' || atomic_mass || ' amu. ' || name 
+                        || ' has a melting point of ' || melting_point_celsius || ' celsius and a boiling point of ' 
+                        || boiling_point_celsius || ' celsius.'
+                from elements left join properties using(atomic_number) 
+                              left join types using(type_id) 
+                where atomic_number =  $1")
   if [[ -z $DATA ]]
   then
     echo I could not find that element in the database.
@@ -27,6 +27,14 @@ else
   then
     ELEMENT_INFO $1
   else
-    echo "Not a number: $1"
+    ATOMIC_NUMBER=$($PSQL "select atomic_number 
+                           from elements 
+                           where name = '$1' or symbol = '$1'")
+      if [[ -z $ATOMIC_NUMBER ]]
+      then
+        echo I could not find that element in the database.
+      else
+        ELEMENT_INFO $ATOMIC_NUMBER
+      fi
   fi
 fi
